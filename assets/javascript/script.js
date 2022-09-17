0255e1521eddc4ca9367f053caec533eaf0e7625
namitaChhantyal <chhantyalnamita111@gmail.com>
2022-09-17 12:13:28 +0545

    navbar active link

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -6,3 +6,42 @@ var typed=new Typed(".typing",{
     loop:true
 
 })
+/* navbar active clickable */
+const nav=document.querySelector(".nav"),
+    navList=nav.querySelectorAll("li"),
+    totalNavList=navList.length,
+    allSection=document.querySelectorAll(".section"),
+    totalSection=allSection.length;
+    for(let i=0; i<totalNavList;i++){
+        const a=navList[i].querySelector("a");
+        a.addEventListener("click",function(){
+            for(let j=0;j<totalNavList;j++){
+                navList[j].querySelector("a").classList.remove("active");
+            }
+            this.classList.add("active")
+            showSection(this);
+            if(window.innerWidth<1200)
+            {
+                asideSectionTogglerBtn();
+            }
+        })
+    }
+    function showSection(element){
+        for(let i=0;i<totalSection;i++){
+            allSection[i].classList.remove("active")
+        }
+        const target=element.getAttribute("href").split("#")[1];
+        document.querySelector("#" + target).classList.add("active")
+    }
+    const navTogglerBtn=document.querySelector(".nav-toggler"),
+        aside=document.querySelector(".aside");
+        navTogglerBtn.addEventListener("click",()=>{
+            asideSectionTogglerBtn();
+        })
+        function asideSectionTogglerBtn(){
+            aside.classList.toggle("open");
+            navTogglerBtn.classList.toggle("open");
+            for(let i=0;i<totalSection;i++){
+                allSection[i].classList.toggle("open")
+            }
+        }
